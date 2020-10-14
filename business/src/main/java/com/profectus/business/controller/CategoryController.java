393561b216cf3dab6a766497e3c8f7fdbb0023e2
xichen@blueplanet.com <xichen@blueplanet.com>
2020-10-14 20:47:36 +1100

    add response dto class and category controller

---
+++ business/src/main/java/com/profectus/business/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.profectus.business.controller;
+
+import com.profectus.business.dto.CategoryDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/business/category")
+public class CategoryController {
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryController.class);
+
+    /**
+     * 列表查询
+     */
+    @PostMapping("/all")
+    public ResponseDto all() {
+        ResponseDto responseDto = new ResponseDto();
+        List<CategoryDto> categoryDtoList = categoryService.all();
+        responseDto.setContent(categoryDtoList);
+        return responseDto;
+    }
+}
