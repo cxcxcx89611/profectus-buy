e41e59fa4e9a796f61e8f2dfb5bed4a4a109a8bf
xichen@blueplanet.com <xichen@blueplanet.com>
2020-10-14 23:07:37 +1100

    fix the context path bug in application.properties

--- business/src/main/java/com/profectus/business/controller/CategoryController.java
+++ business/src/main/java/com/profectus/business/controller/CategoryController.java
@@ -2,14 +2,11 @@ package com.profectus.business.controller;
 
 import com.profectus.business.dto.CategoryDto;
 import com.profectus.business.dto.ResponseDto;
-import com.profectus.business.model.Category;
 import com.profectus.business.service.CategoryService;
-import com.profectus.business.utils.CopyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- business/src/main/java/com/profectus/business/dto/ResponseDto.java
+++ business/src/main/java/com/profectus/business/dto/ResponseDto.java
@@ -2,28 +2,25 @@ package com.profectus.business.dto;
 
 public class ResponseDto<T> {
 
-    /**
-     * 业务上的成功或失败
-     */
+
     private boolean success = true;
 
-    /**
-     * 返回码
-     */
+
     private String code;
 
-    /**
-     * 返回信息
-     */
+
     private String message;
 
-    /**
-     * 返回泛型数据，自定义类型
-     */
+
     private T content;
 
     public String getCode() {
         return code;
+    }
+
+    public ResponseDto() {
+        this.code = "200";
+        this.message = "success";
     }
 
     public void setCode(String code) {
